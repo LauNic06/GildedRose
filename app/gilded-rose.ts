ba98533598b26700ef381981aa43e37b7dc96a2f
Laura Nicoara <Laura.Nicoara@softwire.com>
2022-06-30 08:19:05 +0100

    Done modifing

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,6 +10,18 @@ export class Item {
   }
 }
 
+enum Mode {
+  Increase = 'increase',
+  Decrease = 'decrease'
+}
+
+enum Type {
+  Aged = 'Aged Brie',
+  Backstage = 'Backstage passes to a TAFKAL80ETC concert',
+  Conjured = 'Conjured',
+  Sulfuras = 'Sulfuras, Hand of Ragnaros'
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -17,26 +29,26 @@ export class GildedRose {
     this.items = items;
   }
 
-  modifyQualityBy1(quality: number, mode: string) {
-    if (quality < 50 && mode == 'increase') {
-      quality = quality + 1;
+  modifyQualityBy1(quality: number, mode: Mode) {
+    if (quality < 50 && mode == Mode.Increase) {
+      quality++;
     }
 
-    if (quality > 0 && mode == 'decrease') {
-      quality = quality - 1;
+    if (quality > 0 && mode == Mode.Decrease) {
+      quality--;
     }
 
     return quality;
   }
 
-  modifyQualityBy(n: number, item: Item, mode: string) {
+  modifyQualityBy(n: number, item: Item, mode: Mode) {
     for (let i = 0; i < n; i++) {
       item.quality = this.modifyQualityBy1(item.quality, mode);
     }
   }
 
   updateQualityForAgedBrie(item: Item) {
-    item.quality = this.modifyQualityBy1(item.quality, 'increase');
+    item.quality = this.modifyQualityBy1(item.quality, Mode.Increase);
   }
 
   updateQualityForBackstage(item: Item) {
@@ -46,42 +58,42 @@ export class GildedRose {
     }
 
     if (item.sellIn < 6) {
-      this.modifyQualityBy(3, item, "increase");
+      this.modifyQualityBy(3, item, Mode.Increase);
       return;
     }
 
     if (item.sellIn < 11) {
-      this.modifyQualityBy(2, item, "increase");
+      this.modifyQualityBy(2, item, Mode.Increase);
       return;
     }
 
-    item.quality = this.modifyQualityBy1(item.quality, 'increase');
+    item.quality = this.modifyQualityBy1(item.quality, Mode.Increase);
   }
 
   updateQualityForConjured(item: Item) {
     if (item.sellIn < 0) {
-      this.modifyQualityBy(4, item, "decrease");
+      this.modifyQualityBy(4, item, Mode.Decrease);
       return;
     }
 
-    this.modifyQualityBy(2, item, "decrease");
+    this.modifyQualityBy(2, item, Mode.Decrease);
   }
 
   modifyQualityForItem(item: Item) {
     switch (item.name) {
-      case 'Aged Brie':
+      case Type.Aged:
         this.updateQualityForAgedBrie(item);
         break;
-      case 'Backstage passes to a TAFKAL80ETC concert':
+      case Type.Backstage:
         this.updateQualityForBackstage(item);
         break;
-      case 'Conjured':
+      case Type.Conjured:
         this.updateQualityForConjured(item);
         break;
-      case 'Sulfuras, Hand of Ragnaros':
+      case Type.Sulfuras:
         break;
       default:
-        item.quality = this.modifyQualityBy1(item.quality, 'decrease');
+        item.quality = this.modifyQualityBy1(item.quality, Mode.Decrease);
         break;
     }
   }
@@ -90,10 +102,12 @@ export class GildedRose {
     this.items.forEach(item => {
       this.modifyQualityForItem(item);
 
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
+      if (item.name != Type.Sulfuras) {
+        item.sellIn--;
       }
 
+      // when the sell by day has passed, the quality for
+      // each item will modify accordingly
       if (item.sellIn < 0) {
         this.modifyQualityForItem(item);
       }
